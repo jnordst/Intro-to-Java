7436deee7b6b98182f6cb18809221f5f220eba83
Jacob Nordstrom <levolpehh@hotmail.com>
2023-11-09 09:38:43 -0500

    Added additional lesson to help users get started with Github

---
+++ src/main/java/nordstrom/project/Lesson_00_Getting_Started/Github.java
@@ -0,0 +1,40 @@
+package nordstrom.project.Lesson_00_Getting_Started;
+
+public class Github {
+    // Version control is used to track changes to files and collaborate with other developers.
+
+    // Downlaods:
+        // REQUIRED* Git: https://git-scm.com/downloads
+            // Git is a version control system.
+        // Github Desktop: https://desktop.github.com/
+            // Github is a website that hosts Git repositories.
+            // Github Desktop lets you manage your Git repositories from a handy GUI.
+
+    // How to create a new Github repository using Intelij.
+        // 1 Click on the VCS tab on the top banner (Version Control System)
+        // 2. Click on Share Project on Github
+        // 3. Enter a name for your repository
+        // 4. Select whether you want the repository to be public or private
+        // 5. Click Share
+
+    // How to Commit and Push changes to Github using Intelij.
+        // 1. On the leftside of your screen you should see a tab called Commit.
+        // 2. Click on the Commit tab.
+        // 3. Click on the checkbox next to Changes or manually select the files you want to commit.
+        // 4. Enter a commit message in the Commit Message box.
+        // 5. Click Commit and Push.
+        // 6. An additional popup will appear. Click Push.
+
+    // Commit
+        // A commit is a snapshot of your project at a specific point in time.
+        // Commits are used to save changes to your project to the local repository.
+
+    // Push
+        // Pushing is used to upload your commits to the remote repository.
+        // Pushing is how you share your changes with other developers.
+
+    // Pull
+        // Pulling is used to download changes from the remote repository.
+        // Pulling is how you get the latest changes from other developers.
+
+}
--- src/main/java/nordstrom/project/Main.java
+++ src/main/java/nordstrom/project/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import nordstrom.project.Lesson_02_Classes.Person;
 
 import java.io.IOException;
 
@@ -18,6 +19,16 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        // launch() is used to start the JavaFX application.
+        // Since this project doesn't use JavaFX, we can comment it out.
+        //launch();
+
+        // The main method is the entry point of a Java program.
+        // It is the first method that is called when a Java program is executed.
+        // No other code in any other file will be executed unless it is called from the main method or from a method that is called from the main method.
+        // The launch method is called from the main method.
+        // From the launch method the start method is called which is where the JavaFX application is started.
+        // Most of your code in JavaFX applications will be written in the controller files.
+        // The controller files are called from View (fxml files) or from the Initialize method inside your controllers.
     }
 }
